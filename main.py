284f384151233a94d7c569abd606bd15c252ddf3
Valery Efremov <valeriy131100@gmail.com>
2022-01-12 14:33:01 +0300

    Calculate winery age and paste with jinja2

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from datetime import datetime
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def format_winery_age(age):
+    str_age = str(age)
+
+    if len(str_age) > 1:
+        penultimate_number = int(str_age[-2])
+    else:
+        penultimate_number = 0
+
+    if penultimate_number == 1:
+        return f'{age} лет'
+
+    last_number = int(str_age[-1])
+
+    if last_number == 1:
+        return f'{age} год'
+    elif 1 < last_number < 5:
+        return f'{age} года'
+    else:
+        return f'{age} лет'
+
+
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+
+    now_year = datetime.now().year
+    foundation_year = 1920
+    winery_age = now_year - foundation_year
+
+    rendered_page = template.render(
+        formatted_age=format_winery_age(winery_age)
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
